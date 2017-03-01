6f17a5675652186542db6481f9f7e069a95ac9ee
tiago9864 <santiago2091@gmail.com>
2017-03-01 07:59:06 +0300

    added git init

---
+++ index.js
@@ -0,0 +1,17 @@
+var express=require('express');
+var server=express();
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(request,response){
+  response.sendFile('public/html/index.html', {root:__dirname});
+
+});
+server.get('/signup', function(request,response){
+  response.sendFile('public/html/signup.html',{root:__dirname});
+});
+server.get('/login', function(request,response){
+  response.sendFile('public/html/login.html', {root:__dirname});
+});
+server.listen(8080, function(){
+  console.log("now listening on..", 8080);
+});
